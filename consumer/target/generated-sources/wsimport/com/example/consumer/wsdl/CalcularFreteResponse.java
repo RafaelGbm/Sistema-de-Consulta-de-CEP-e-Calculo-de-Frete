8fb49ef510ba71a7c330ae03dfc3ba0ff5ed7a06
Rafael <rafaelgasparmartins@icloud.com>
2026-03-25 01:46:54 -0300

    feat: geração automática das classes Java do consumer a partir dos WSDLs do producer

---
+++ consumer/target/generated-sources/wsimport/com/example/consumer/wsdl/CalcularFreteResponse.java
@@ -0,0 +1,84 @@
+
+package com.example.consumer.wsdl;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de anonymous complex type.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="valor" type="{http://www.w3.org/2001/XMLSchema}decimal"/&gt;
+ *         &lt;element name="prazo" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "valor",
+    "prazo"
+})
+@XmlRootElement(name = "calcularFreteResponse")
+public class CalcularFreteResponse {
+
+    @XmlElement(required = true)
+    protected BigDecimal valor;
+    protected int prazo;
+
+    /**
+     * Obtém o valor da propriedade valor.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    /**
+     * Define o valor da propriedade valor.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setValor(BigDecimal value) {
+        this.valor = value;
+    }
+
+    /**
+     * Obtém o valor da propriedade prazo.
+     * 
+     */
+    public int getPrazo() {
+        return prazo;
+    }
+
+    /**
+     * Define o valor da propriedade prazo.
+     * 
+     */
+    public void setPrazo(int value) {
+        this.prazo = value;
+    }
+
+}
